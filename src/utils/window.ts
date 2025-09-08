11dbbe8795d8060c3562865e266908964f365ea5
AlexGearX <alexis.lutran@gmail.com>
2025-09-08 10:01:47 +0200

    🏷️ Migrate window.js to window.ts

---
+++ src/utils/window.ts
@@ -0,0 +1,11 @@
+export const refreshPage = () => window.location.reload()
+
+export const setLocalStorage = <T>(key: string, value: T) => {
+  localStorage.setItem(key, JSON.stringify(value))
+}
+
+export const getLocalStorage = <T>(key: string): T | null => {
+  const item = localStorage.getItem(key)
+  if (!item) return null
+  return JSON.parse(item)
+}
