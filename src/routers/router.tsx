c808b7a5d0cb8e9dd829cec83ce7f218ee480cc0
AlexGearX <alexis.lutran@gmail.com>
2025-09-14 12:52:32 +0200

    ♻️ Add alias import in router.tsx

--- src/routers/router.tsx
+++ src/routers/router.tsx
@@ -1,7 +1,7 @@
+import ErrorPage from '@/components/pages/error/ErrorPage'
+import LoginPage from '@/components/pages/login/LoginPage'
+import OrderPage from '@/components/pages/order/OrderPage'
 import { createBrowserRouter } from 'react-router-dom'
-import LoginPage from '../components/pages/login/LoginPage'
-import OrderPage from '../components/pages/order/OrderPage'
-import ErrorPage from '../components/pages/error/ErrorPage'
 
 const routes = [
   {
