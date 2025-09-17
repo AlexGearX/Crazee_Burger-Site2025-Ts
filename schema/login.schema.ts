23e632810b886b067ee3de296cb1653613054aea
AlexGearX <alexis.lutran@gmail.com>
2025-09-17 21:11:18 +0200

    ♻️ Update zod shema

--- schema/login.schema.ts
+++ schema/login.schema.ts
@@ -1,12 +1,23 @@
 import { z } from 'zod'
 
+const MAX_USERNAME_LENGTH = 20
+const MIN_USERNAME_LENGTH = 2
+const USERNAME_REGEX = /^[a-zA-Z-]+$/
+
+const errorMessages = {
+  empty: 'Veuillez entrer un prénom',
+  min: `Le prénom doit contenir au moins ${MIN_USERNAME_LENGTH} caractères`,
+  max: `Le prénom ne doit pas dépasser ${MAX_USERNAME_LENGTH} caractères`,
+  regex: 'Le prénom ne doit contenir que des lettres ou -',
+}
+
 export const loginSchema = z.object({
   username: z
     .string()
-    .nonempty('Veuillez entrer un prénom')
-    .min(2, 'Le prénom doit contenir au moins 2 caractères')
-    .max(20, 'Le prénom ne doit pas dépasser 20 caractères')
-    .regex(/^[a-zA-Z-]+$/, 'Le prénom ne doit contenir que des lettres ou -'),
+    .nonempty(errorMessages.empty)
+    .min(MIN_USERNAME_LENGTH, errorMessages.min)
+    .max(MAX_USERNAME_LENGTH, errorMessages.max)
+    .regex(USERNAME_REGEX, errorMessages.regex),
 })
 
 export type LoginFormData = z.infer<typeof loginSchema>
